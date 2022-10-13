b3d96b8fc2cac5686183b0f592260e5e6eb92794
Deveshb15 <deveshb583@gmail.com>
2022-10-13 20:47:07 +0530

    feat: dynamic meta og

--- pages/github/[id].jsx
+++ pages/github/[id].jsx
@@ -1,56 +1,79 @@
-import Head from 'next/head'
+import Head from "next/head";
 
-export const getServerSideProps = async({ query }) => {
-    // console.log(query.id)
+export const getServerSideProps = async ({ query }) => {
+	// console.log(query.id)
 
-    const response = await fetch(`https://api.github.com/users/${query.id}`)
-    const data = await response.json()
-    // console.log(data)
-    let name = data?.name ?? "Not found"
-    let bio = data?.bio ?? "Not found"
-    let img_url = data?.avatar_url ?? "https://avatars.githubusercontent.com/u/72264218?v=4"
-    return {
-        props: {
-            name,
-            bio,
-            img_url
-        }
-    }
-} 
+	const response = await fetch(`https://api.github.com/users/${query.id}`);
+	const data = await response.json();
+	// console.log(data)
+	let name = data?.name ?? "Not found";
+	let bio = data?.bio ?? "Not found";
+	let img_url =
+		data?.avatar_url ?? "https://avatars.githubusercontent.com/u/72264218?v=4";
+	let username = data?.login ?? "Deveshb15";
+	return {
+		props: {
+			name,
+			bio,
+			img_url,
+			username,
+		},
+	};
+};
 
-const GithubUser= ({ name , bio, img_url }) => {
-  return (
-    <div>
-      <Head>
-        <title>{name}</title>
-        <meta name="description" content="Dynamic Github OG based on your username" />
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="og:image"
-          content={
-            `${
-              process.env.VERCEL_URL ? 'https://' + process.env.VERCEL_URL : 'http://localhost:3000'
-            }/api/git?name=${name}&bio=${bio}&imageurl=${img_url}`
-          }
-        />
-      </Head>
+const GithubUser = ({ name, bio, img_url, username }) => {
+	return (
+		<div>
+			<Head>
+				<title>{name}</title>
+				<meta
+					name="description"
+					content="Dynamic Github OG based on your username"
+				/>
+				<link rel="icon" href="/favicon.ico" />
+				<meta
+					name="og:image"
+					content={`${
+						process.env.VERCEL_URL
+							? "https://" + process.env.VERCEL_URL
+							: "http://localhost:3000"
+					}/api/git?name=${name}&bio=${bio}&imageurl=${img_url}`}
+				/>
+			</Head>
 
-      <main className="flex min-h-screen justify-center items-center">
-        <div className='flex flex-col'>
-            <h1 className="text-2xl font-bold">{name}</h1>
-            <p className="text-lg font-normal">{bio}</p>
-        </div>
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img alt="avatar"
-          width="256"
-          src={img_url}
-          style={{
-            borderRadius: 128,
-          }} />
-      </main>
+			<main className="flex min-h-screen justify-center items-center">
+				<div className="flex flex-col">
+					<div className="flex justify-center items-center">
+						<div className="flex flex-col">
+							<h1 className="text-2xl font-bold">{name}</h1>
+							<p className="text-lg font-normal">{bio}</p>
+						</div>
+						{/* eslint-disable-next-line @next/next/no-img-element */}
+						<img
+							alt="avatar"
+							width="256"
+							src={img_url}
+							style={{
+								borderRadius: 128,
+							}}
+						/>
+					</div>
+					<h1 className="text-6xl font-extrabold">
+						Link :{" "}
+						<a
+							href={`${
+								process.env.VERCEL_URL
+									? process.env.VERCEL_URL
+									: "http://localhost:3000"
+							}/github/${username}`}
+						>
+							{name}
+						</a>
+					</h1>
+				</div>
+			</main>
+		</div>
+	);
+};
 
-    </div>
-  )
-}
-
-export default GithubUser
+export default GithubUser;
